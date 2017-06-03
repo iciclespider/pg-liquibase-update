a329b0322379b02945e390e4539c8f37b530742f
Patrick J. McNerthney <pmcnerthney@tripwire.com>
2017-06-02 18:03:47 -1000

    Perform the liquibase update in the changelog directory.

--- update.sh
+++ update.sh
@@ -4,30 +4,32 @@ echo "Setting up liquidbase"
 : ${POSTGRES_USER?"POSTGRES_USER not set"}
 : ${POSTGRES_PASSWORD?"POSTGRES_PASSWORD not set"}
 
-cat <<CONF > /home/duser/liquibase.properties
-  driver: org.postgresql.Driver
-  classpath:/opt/jdbc_drivers/postgresql-9.3-1102-jdbc41.jar
-  url: jdbc:postgresql://$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB
-  username: $POSTGRES_USER
-  password: $POSTGRES_PASSWORD
-CONF
-
 echo "Applying changelogs ..."
 : ${CHANGELOG_FILE:="changelogs.xml"}
+
+cd /changelogs
 
 ERROR_EXIT_CODE=1
 MAX_TRIES=${MAX_TRIES:-4}
 COUNT=1
 while [  $COUNT -le $MAX_TRIES ]; do
    echo  "Attempting to apply changelogs: attempt $COUNT of $MAX_TRIES"
-   liquibase --changeLogFile="/changelogs/$CHANGELOG_FILE" update
+   liquibase \
+       --driver=org.postgresql.Driver \
+       --classpath=/opt/jdbc_drivers/postgresql-9.3-1102-jdbc41.jar \
+       --url="jdbc:postgresql://$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB" \
+       --username="$POSTGRES_USER" \
+       --password="$POSTGRES_PASSWORD" \
+       --changeLogFile="$CHANGELOG_FILE" \
+       update
    if [ $? -eq 0 ];then
-   	  echo "Changelogs successfully applied"
-      exit 0
+       echo "Changelogs successfully applied"
+       exit 0
    fi
    echo "Failed to apply changelogs"
    sleep 1
    let COUNT=COUNT+1
 done
+
 echo "Too many non-successful tries"
 exit $ERROR_EXIT_CODE
